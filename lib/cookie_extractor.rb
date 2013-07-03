eda67c7ea8b059dfbf6517d401c20cb43d9c6977
Jeff Dallien <jeff@dallien.net>
2013-07-02 23:07:52 -0400

    Fix load path.

--- lib/cookie_extractor.rb
+++ lib/cookie_extractor.rb
@@ -1,3 +1,6 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
 require "cookie_extractor/version"
 require "cookie_extractor/common"
 require "cookie_extractor/firefox_cookie_extractor"
